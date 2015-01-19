0875950f00c09975f7ddc1938c00ae1734ffd313
ashwanikhare <ashwanikhare.nitw@gmail.com>
2015-01-19 12:38:35 +0000

    Add cyk.cpp

---
+++ cyk.cpp
@@ -0,0 +1,18 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+vector< vector<string> > table;
+map<char,vector<string> grammar;
+
+
+int main(int argc, char const *argv[])
+{
+	string s;
+	cin>>s;
+	while(s!='#')
+	{
+		
+	}
+	return 0;
+}
